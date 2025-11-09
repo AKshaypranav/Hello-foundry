419376ae5f92dd9839c45ad08e6323f3d87ad444
prime <akshaypranav17@gmail.com>
2025-11-09 07:10:27 -0500

    ProjectSetup in solidity

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.19;
+import "forge-std/Script.sol";
+import "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simplestorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simplestorage;  
+    }
+}
